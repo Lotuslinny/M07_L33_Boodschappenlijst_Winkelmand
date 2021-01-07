b727345f30b0f02a03d999eac7c2eb6f3c7dac0a
Lotuslinny <linny@fegg.nl>
2021-01-07 16:22:39 +0100

    Boodschappenlijst start

---
+++ src/components/List.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+class List extends React.Component {
+  render() {
+    return (
+      <div>
+        <ul>
+          <li>Groenten</li>
+          <li>Rijst</li>
+          <li>Melk</li>
+          <li>Brood</li>
+          <li>Vlokken</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default List
